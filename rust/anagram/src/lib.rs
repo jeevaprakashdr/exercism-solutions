8c3a7b89cb927e48f963c29edbcba26fa0ed2dcb
jeeva <jeevaprakashdr@gmail.com>
2024-01-19 15:01:19 +0100

    fix test anagrams_must_use_all_letters_exactly_once

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&'a str]) -> HashSet<&'a str> {
     let mut result: HashSet<&'a str> = HashSet::new();
-
+    
     for possible_anagram in possible_anagrams {
         let mut is_word_a_match = true;
         if possible_anagram.chars().count() != word.chars().count() {
@@ -13,15 +13,14 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&'a str]) -> HashSet
             continue;
         }
 
-        let mut processed = HashSet::new();
         for c in word.to_lowercase().chars() {
             
-            processed.contains(&c);
-            if !possible_anagram.to_lowercase().contains(c) {
+            let char_count_in_word = word.to_lowercase().chars().filter(|x| x.eq(&c)).count();
+            let char_count_in_anagram = possible_anagram.to_lowercase().chars().filter(|x| x.eq(&c)).count();
+
+            if !possible_anagram.to_lowercase().contains(c) || char_count_in_anagram > char_count_in_word{
                 is_word_a_match = false
             }
-
-            processed.insert(c);
         }
 
         if is_word_a_match {

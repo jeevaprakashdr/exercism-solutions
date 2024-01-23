6ad329e6568fcd71b6cc73a51e2813cd9abe581a
jeeva <jeevaprakashdr@gmail.com>
2024-01-23 23:03:05 +0100

    clocks comparing

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -11,31 +11,18 @@ impl Clock {
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
+        let clock = Self::calculate_hours_and_minutes(self.hours, self.minutes + minutes);
         Clock {
-            hours: self.hours,
-            minutes: self.minutes + minutes,
+            hours: clock.0,
+            minutes: clock.1,
         }
     }
 
     fn get_hours_and_minutes(&self) -> (String, String) {
-        let mut hours = self.hours;
-        let mut minutes = self.minutes;
-
-        if minutes < 0 {
-            minutes = self.minutes.rem_euclid(60);
-            hours += self.minutes.div_euclid(60);
-        } else if minutes >= 60 {
-            let val: f32 = self.minutes as f32 / 60.0;
-            hours += val.trunc() as i32;
-            minutes = self.minutes.rem_euclid(60)
-        }
-
-        if hours < 0 {
-            hours = 24 + (hours % 24);
-        } else if hours > 24 {
-            hours = hours % 24
-        }
-
+        let clock = Self::calculate_hours_and_minutes(self.hours, self.minutes);
+        let hours = clock.0;
+        let minutes = clock.1;
+        
         let hour_str = match hours {
             0..=9 => format!("0{}", hours),
             10..=23 => hours.to_string(),
@@ -45,13 +32,47 @@ impl Clock {
 
         let minute_str = match minutes {
             1..=9 => format!("0{}", minutes),
-            10..=58 => minutes.to_string(),
-            0 | 59 => "00".to_owned(),
-            60 => "00".to_owned(),
+            10..=59 => minutes.to_string(),
+            0 | 60 => "00".to_owned(),
             _ => panic!("Invalid minutes"),
         };
 
         (hour_str, minute_str)
+    }
+
+    fn calculate_hours_and_minutes(h: i32, m:i32 ) -> (i32, i32) {
+        let mut hours = h;
+        let mut minutes = m;
+        
+        if minutes < 0 {
+            minutes = m.rem_euclid(60);
+            hours += m.div_euclid(60);
+        } else if minutes >= 60 {
+            let val: f32 = m as f32 / 60.0;
+            hours += val.trunc() as i32;
+            minutes = m.rem_euclid(60)
+        }
+
+        if hours < 0 {
+            hours = 24 + (hours % 24);
+        } else if hours > 24 {
+            hours = hours % 24
+        }
+
+        hours = match hours {
+            0..=23 => hours,
+            24 => 0,
+            _ => panic!("Invalid hours"),
+        };
+
+        minutes = match minutes {
+            1..=9 => minutes,
+            10..=59 => minutes,
+            0 | 60 => 0,
+            _ => panic!("Invalid minutes"),
+        };
+        
+        (hours, minutes)
     }
 }
 

04f68c7963697e48c31546224929152634035162
jeeva <jeevaprakashdr@gmail.com>
2024-01-23 21:56:46 +0100

    fix add hours tests

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -11,35 +11,38 @@ impl Clock {
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
-    }   
+        Clock {
+            hours: self.hours,
+            minutes: self.minutes + minutes,
+        }
+    }
 
     fn get_hours_and_minutes(&self) -> (String, String) {
         let mut hours = self.hours;
-        let mut minutes= self.minutes ;
+        let mut minutes = self.minutes;
 
         if minutes < 0 {
-            minutes = self.minutes.rem_euclid(60);            
+            minutes = self.minutes.rem_euclid(60);
             hours += self.minutes.div_euclid(60);
         } else if minutes >= 60 {
-            let val: f32 = self.minutes as f32/ 60.0;
+            let val: f32 = self.minutes as f32 / 60.0;
             hours += val.trunc() as i32;
-            minutes =  self.minutes.rem_euclid(60)
+            minutes = self.minutes.rem_euclid(60)
         }
-        
+
         if hours < 0 {
-            hours = 24 + (hours % 24); 
+            hours = 24 + (hours % 24);
         } else if hours > 24 {
             hours = hours % 24
         }
-        
+
         let hour_str = match hours {
             0..=9 => format!("0{}", hours),
             10..=23 => hours.to_string(),
             24 => "00".to_owned(),
             _ => panic!("Invalid hours"),
         };
-        
+
         let minute_str = match minutes {
             1..=9 => format!("0{}", minutes),
             10..=58 => minutes.to_string(),
@@ -50,7 +53,6 @@ impl Clock {
 
         (hour_str, minute_str)
     }
-
 }
 
 impl fmt::Display for Clock {

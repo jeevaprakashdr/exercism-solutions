161008a35e06873285ebdd734763b48b742f0b7c
jeeva <jeevaprakashdr@gmail.com>
2024-01-24 15:21:34 +0100

    refactor to use filter efficiently

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,37 +1,19 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&'a str]) -> HashSet<&'a str> {
-    let mut result: HashSet<&'a str> = HashSet::new();
+    let word_lowercase = word.to_lowercase();
+    let mut word_lowercase_vec = word_lowercase.chars().collect::<Vec<char>>();
+    word_lowercase_vec.sort();
 
-    let filtered_anagrams: Vec<&'a str> = possible_anagrams
+    possible_anagrams
         .iter()
         .filter(|x| x.chars().count() == word.chars().count())
-        .filter(|x| x.to_lowercase() != word.to_lowercase())
+        .filter(|x| x.to_lowercase() != word_lowercase)
+        .filter(|anagrams| {
+            let mut sorted_anagram = anagrams.to_lowercase().chars().collect::<Vec<char>>();
+            sorted_anagram.sort();
+            sorted_anagram == word_lowercase_vec
+        })
         .map(|x| *x)
-        .collect();
-    
-    for possible_anagram in filtered_anagrams {
-        let mut is_word_a_match = true;
-       
-        for c in word.to_lowercase().chars() {
-            let char_count_in_word = word.to_lowercase().chars().filter(|x| x.eq(&c)).count();
-            let char_count_in_anagram = possible_anagram
-                .to_lowercase()
-                .chars()
-                .filter(|x| x.eq(&c))
-                .count();
-
-            if !possible_anagram.to_lowercase().contains(c)
-                || char_count_in_anagram > char_count_in_word
-            {
-                is_word_a_match = false
-            }
-        }
-
-        if is_word_a_match {
-            result.insert(possible_anagram);
-        }
-    }
-
-    result
+        .collect()
 }

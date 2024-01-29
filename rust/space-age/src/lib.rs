8e4864cdf37202153c5bea05fade3b9285fc5513
jeeva <jeevaprakashdr@gmail.com>
2024-01-29 15:01:14 +0100

    fix venus earth year

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -16,7 +16,7 @@ impl From<u64> for Duration {
 
 pub trait Planet {
     fn years_during(d: &Duration) -> f64 {
-        (1.0/31557600.0) * d.seconds as f64
+        todo!("convert a duration ({d:?}) to the number of years on this planet for that duration");
     }
 }
 
@@ -29,6 +29,13 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
+impl Planet for Earth {
+    fn years_during(d: &Duration) -> f64 {
+        let earth_year = 31557600.0 * 1.0 as f64;
+        (1.0/earth_year) * d.seconds as f64
+    }
+}
+
 impl Planet for Mercury {
     fn years_during(d: &Duration) -> f64 {
         let mercury_earth_year = 31557600.0 * 0.2408467 as f64;
@@ -36,8 +43,13 @@ impl Planet for Mercury {
     }
 }
 
-impl Planet for Venus {}
-impl Planet for Earth {}
+impl Planet for Venus {
+    fn years_during(d: &Duration) -> f64 {
+        let venus_earth_year = 31557600.0 * 0.61519726 as f64;
+        (1.0/venus_earth_year) * d.seconds as f64
+    }
+}
+
 impl Planet for Mars {}
 impl Planet for Jupiter {}
 impl Planet for Saturn {}

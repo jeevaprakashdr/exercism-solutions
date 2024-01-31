0a8ec0518864968ef8d8ab05bf70758e1b8447aa
jeeva <jeevaprakashdr@gmail.com>
2024-01-31 12:21:18 +0100

    introduce macros

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,11 +1,12 @@
 // The code below is a stub. Just enough to satisfy the compiler.
 // In order to pass the tests you can add-to or change any of this code.
 
+const EARTH_YEAR_IN_SECONDS:f64 = 31557600.0;
 #[derive(Debug)]
 pub struct Duration {
     seconds: u64
 }
-
+ 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
         Duration {
@@ -29,26 +30,22 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Earth {
-    fn years_during(d: &Duration) -> f64 {
-        let earth_year = 31557600.0 * 1.0 as f64;
-        (1.0/earth_year) * d.seconds as f64
-    }
+macro_rules! planet_impl {
+    (
+        $planet_name: ident,
+        $orbital_period: expr
+    ) => {
+        impl Planet for $planet_name {
+            fn years_during(d: &Duration) -> f64 {
+                let earth_year: f64 = EARTH_YEAR_IN_SECONDS * $orbital_period as f64;
+                (1.0/earth_year) * d.seconds as f64
+            }
+        }        
+    };
 }
-
-impl Planet for Mercury {
-    fn years_during(d: &Duration) -> f64 {
-        let mercury_earth_year = 31557600.0 * 0.2408467 as f64;
-        (1.0/mercury_earth_year) * d.seconds as f64
-    }
-}
-
-impl Planet for Venus {
-    fn years_during(d: &Duration) -> f64 {
-        let venus_earth_year = 31557600.0 * 0.61519726 as f64;
-        (1.0/venus_earth_year) * d.seconds as f64
-    }
-}
+planet_impl!(Earth, 1.0);
+planet_impl!(Mercury, 0.2408467);
+planet_impl!(Venus, 0.61519726);
 
 impl Planet for Mars {}
 impl Planet for Jupiter {}
